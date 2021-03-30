9bc3b37df8316415340d9103749527fb5ea7ac6d
Mary Vogler <mvogler@umich.edu>
2021-03-30 13:18:09 -0400

    starting out

--- js/video.js
+++ js/video.js
@@ -1,9 +1,33 @@
-var video;
+var video = document.getElementsByClassName('video');
+
+
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
 
 });
+
+function playVid() {
+    video.play();
+    var vol = document.getElementById('volume');
+    document.querySelector("video").volume = vol;
+}
+
+function pauseVid() {
+    video.pause();
+}
+function slow() {
+
+}
+
+function fast() {
+    
+}
+
+function skip() {
+    
+}
+
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
