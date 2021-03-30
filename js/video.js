037bd5fa7b36e552a5af098b0a9448a2baf05081
Mary Vogler <mvogler@umich.edu>
2021-03-30 15:00:31 -0400

    link

--- js/video.js
+++ js/video.js
@@ -1,4 +1,4 @@
-var video = document.getElementsByClassName('video');
+var vid = document.getElementsByClassName('video');
 
 
 
@@ -8,26 +8,56 @@ window.addEventListener("load", function() {
 });
 
 function playVid() {
-    video.play();
+    vid.play();
     var vol = document.getElementById('volume');
     document.querySelector("video").volume = vol;
 }
 
 function pauseVid() {
-    video.pause();
+    vid.pause();
 }
 function slow() {
-
+    vid.playbackRate = 0.95;
+    console.log(vid.playbackRate);
 }
 
 function fast() {
+    vid.playbackRate = 1.05;
+    console.log(vid.playbackRate);
     
 }
 
 function skip() {
-    
+    var start = vid.currentTime;
+    var end = vid.duration;
+    if (end - start > 15) {
+        vid.currentTime = vid.currentTime = 15;
+        console.log(vid.currentTime);
+        vid.play();
+    } else {
+        vid.currentTime = 0;
+        console.log(vid.currentTime);
+        vid.play();
+    }
+
 }
 
+function mute() {
+    if (vid.muted == true) {
+        vid.muted = false;
+        document.getElementById('mute').innerHTML = 'Unmuted';
+    } else {
+        vid.muted = true;
+        document.getElementById('mute').innerHTML = 'Muted'
+
+    }
+}
+
+function slide() {
+    vid.volume = vid.volume + 10;
+    document.getElementById('volume').innerHTML = vid.volume;
+    
+}
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
